5dfd1aecdca2556d2010ff70e7a76ad9bcbe0f0e
David de la Rosa Delgado <drd_id@hotmail.com>
2022-01-05 22:01:59 +0100

    NEW PROJECT: poc-springboot-micro-productos

---
+++ poc-springboot-micro-productos/src/main/java/org/delard/poc/springboot/micro/app/productos/PocSpringbootMicroProductosApplication.java
@@ -0,0 +1,13 @@
+package org.delard.poc.springboot.micro.app.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringbootMicroProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringbootMicroProductosApplication.class, args);
+	}
+
+}
---
+++ poc-springboot-micro-productos/src/test/java/org/delard/poc/springboot/micro/app/productos/PocSpringbootMicroProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.delard.poc.springboot.micro.app.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSpringbootMicroProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
