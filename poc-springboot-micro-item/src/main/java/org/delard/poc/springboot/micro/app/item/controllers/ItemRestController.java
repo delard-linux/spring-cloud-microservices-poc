da631b800af7aef56902661dbc68c0cdb692e014
David de la Rosa Delgado <drd_id@hotmail.com>
2022-01-06 23:05:11 +0100

    16. Creando controlador rest en items

---
+++ poc-springboot-micro-item/src/main/java/org/delard/poc/springboot/micro/app/item/controllers/ItemRestController.java
@@ -0,0 +1,35 @@
+package org.delard.poc.springboot.micro.app.item.controllers;
+
+import java.util.List;
+
+import org.delard.poc.springboot.micro.app.item.models.Item;
+import org.delard.poc.springboot.micro.app.item.models.service.IItemsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class ItemRestController {
+	
+	@Autowired
+	private IItemsService itemService;
+	
+	@GetMapping("/listar")
+	public List<Item> listar() {
+		
+		return itemService.findAll();
+						
+	}
+	
+	@GetMapping("/ver/{id}/cantidad/{cantidad}")
+	public Item detail(@PathVariable Long id, @PathVariable Integer cantidad ) {
+		
+		return itemService.findById(id, cantidad);
+		
+	}	
+	
+}
