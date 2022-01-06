ff1e4af01df10da98735e4b92fd9ab1f2db4b5dc
David de la Rosa Delgado <drd_id@hotmail.com>
2022-01-06 20:50:16 +0100

    NEW PROJECT: poc-springboot-micro-item

---
+++ poc-springboot-micro-item/src/main/java/org/delard/poc/springboot/micro/app/item/PocSpringbootMicroItemApplication.java
@@ -0,0 +1,13 @@
+package org.delard.poc.springboot.micro.app.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringbootMicroItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringbootMicroItemApplication.class, args);
+	}
+
+}
---
+++ poc-springboot-micro-item/src/test/java/org/delard/poc/springboot/micro/app/item/PocSpringbootMicroItemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.delard.poc.springboot.micro.app.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSpringbootMicroItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
