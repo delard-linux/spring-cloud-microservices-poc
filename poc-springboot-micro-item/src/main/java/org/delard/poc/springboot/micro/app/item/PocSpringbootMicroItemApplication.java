8f3af3b8cecc1d402caaf1e7206e4ace23c3d04d
David de la Rosa Delgado <drd_id@hotmail.com>
2022-01-07 18:18:15 +0100

    23. Dependencias Eureka Client y habilitar los servicios
    Se dejan las versions de Spring y Cloud a la última de nuevo. Se
    habilitan los dos servicios como Eureka y se elimina la dependencia de
    Ribbon

--- poc-springboot-micro-item/src/main/java/org/delard/poc/springboot/micro/app/item/PocSpringbootMicroItemApplication.java
+++ poc-springboot-micro-item/src/main/java/org/delard/poc/springboot/micro/app/item/PocSpringbootMicroItemApplication.java
@@ -2,10 +2,10 @@ package org.delard.poc.springboot.micro.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name="servicio-productos")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class PocSpringbootMicroItemApplication {
