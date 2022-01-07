feff90c837c2103d3e0fafc2eed7fd614e67e87a
David de la Rosa Delgado <drd_id@hotmail.com>
2022-01-07 11:59:21 +0100

    18. Balanceo de carga del lado del cliente con Ribbon
    Como Ribbon ya está deprecado en las versiones a partir de SpringBoot
    2.4 hay que hacer un downgrade a 2.3 para esta PoC

--- poc-springboot-micro-item/src/main/java/org/delard/poc/springboot/micro/app/item/clientes/ProductoClienteRest.java
+++ poc-springboot-micro-item/src/main/java/org/delard/poc/springboot/micro/app/item/clientes/ProductoClienteRest.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="servicio-productos",url="localhost:8001")
+@FeignClient(name="servicio-productos")
 public interface ProductoClienteRest {
 	
 	@GetMapping("/listar")
