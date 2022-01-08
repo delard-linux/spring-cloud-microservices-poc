9d5729785a77a1d177e3a4970dc57be86abbd27f
David de la Rosa Delgado <drd_id@hotmail.com>
2022-01-08 12:41:37 +0100

    NEW PROJECT: poc-springboot-micro-zuul-server

---
+++ poc-springboot-micro-zuul-server/src/main/java/org/delard/poc/springboot/micro/app/zuul/PocSpringbootMicroZuulServerApplication.java
@@ -0,0 +1,13 @@
+package org.delard.poc.springboot.micro.app.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringbootMicroZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringbootMicroZuulServerApplication.class, args);
+	}
+
+}
