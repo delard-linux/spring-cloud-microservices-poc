7cd45f5dcb52444074fd2370cbf9c7b248210f61
David de la Rosa Delgado <drd_id@hotmail.com>
2022-01-08 13:52:47 +0100

    29. Zuul Filtro HTTP post - Tiempo transcurrido

---
+++ poc-springboot-micro-zuul-server/src/main/java/org/delard/poc/springboot/micro/app/zuul/filters/PostTiempoTranscurridoFilter.java
@@ -0,0 +1,47 @@
+package org.delard.poc.springboot.micro.app.zuul.filters;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class PostTiempoTranscurridoFilter extends ZuulFilter{
+
+	private static Logger log = LoggerFactory.getLogger(PostTiempoTranscurridoFilter.class);
+	
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		
+		Long tiempoFinal = System.currentTimeMillis();
+		Long tiempoInicio = (Long)request.getAttribute("tiempoInicio");
+
+		log.info(String.format("Tiempo transcurrido %s",tiempoFinal-tiempoInicio));
+		
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "post";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
