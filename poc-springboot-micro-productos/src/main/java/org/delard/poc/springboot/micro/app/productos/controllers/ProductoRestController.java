a06c5494322de6e5d66c756336416e266af7dd59
David de la Rosa Delgado <drd_id@hotmail.com>
2022-01-08 13:17:34 +0100

    Visualizar port real con Environment
    en vez de @Value (no funciona con puerto dinamico) visualizaba 0

--- poc-springboot-micro-productos/src/main/java/org/delard/poc/springboot/micro/app/productos/controllers/ProductoRestController.java
+++ poc-springboot-micro-productos/src/main/java/org/delard/poc/springboot/micro/app/productos/controllers/ProductoRestController.java
@@ -1,13 +1,12 @@
 package org.delard.poc.springboot.micro.app.productos.controllers;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.delard.poc.springboot.micro.app.productos.models.entity.Producto;
 import org.delard.poc.springboot.micro.app.productos.models.service.IProductoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,17 +19,17 @@ public class ProductoRestController {
 	@Autowired
 	private IProductoService productoService;
 	
-	@Value("${server.port}")
-	private Integer port;
+	@Autowired
+	private Environment env; 
 	
 	@GetMapping("/listar")
 	public List<Producto> listar() {
 		
 		return productoService.findAll().stream().map(
 				producto -> {
-					producto.setPort(port);
+					producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 					return producto;
-				}).collect(Collectors.toList());
+				}).toList();
 						
 	}
 	
@@ -38,7 +37,7 @@ public class ProductoRestController {
 	public Producto detail(@PathVariable Long id) {
 		
 		var producto = productoService.findById(id);
-		producto.setPort(port);
+		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		return producto;
 		
 	}	
