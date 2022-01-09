7ac01909bed5a3fd22a0161ff463c90046fa3b64
David de la Rosa Delgado <drd_id@hotmail.com>
2022-01-09 22:13:51 +0100

    39. Filtros Gateway Factory que vienen de fábrica
    Se modifica un controller de Items para sacar por consola el valor de
    los request param y headers de la request que solo van en la ida

--- poc-springboot-micro-item/src/main/java/org/delard/poc/springboot/micro/app/item/controllers/ItemRestController.java
+++ poc-springboot-micro-item/src/main/java/org/delard/poc/springboot/micro/app/item/controllers/ItemRestController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -22,7 +24,14 @@ public class ItemRestController {
 	private IItemsService itemService;
 	
 	@GetMapping("/listar")
-	public List<Item> listar() {
+	public List<Item> listar(
+				@RequestParam(name="nombre") String nombre,
+				@RequestHeader(name="token-request") String token
+				) {
+		
+		System.out.println("Configuracion por filtro de fabrica: ");
+		System.out.println(" nombre = " + nombre);
+		System.out.println(" token-request = " + token);
 		
 		return itemService.findAll();
 						
