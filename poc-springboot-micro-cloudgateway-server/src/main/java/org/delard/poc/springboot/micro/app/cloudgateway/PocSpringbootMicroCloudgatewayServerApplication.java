3835cb3c67741764e9e1acae9d992a98db12f2a4
David de la Rosa Delgado <drd_id@hotmail.com>
2022-01-09 12:20:26 +0100

    NEW PROJECT: poc-springboot-micro-cloudgateway-server

---
+++ poc-springboot-micro-cloudgateway-server/src/main/java/org/delard/poc/springboot/micro/app/cloudgateway/PocSpringbootMicroCloudgatewayServerApplication.java
@@ -0,0 +1,13 @@
+package org.delard.poc.springboot.micro.app.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringbootMicroCloudgatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringbootMicroCloudgatewayServerApplication.class, args);
+	}
+
+}
