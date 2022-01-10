e1d1a3f6e80926bfd25a7e9ce75b60f69dfcb91a
David de la Rosa Delgado <drd_id@hotmail.com>
2022-01-10 22:35:13 +0100

    46. Perzonalizando parámetros del Circuit Breaker

--- poc-springboot-micro-item/src/main/java/org/delard/poc/springboot/micro/app/item/AppConfig.java
+++ poc-springboot-micro-item/src/main/java/org/delard/poc/springboot/micro/app/item/AppConfig.java
@@ -1,9 +1,17 @@
 package org.delard.poc.springboot.micro.app.item;
 
+import java.time.Duration;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 @Configuration
 public class AppConfig {
@@ -13,6 +21,22 @@ public class AppConfig {
 	public RestTemplate registrarRestTemplate() {
 		return new RestTemplate();
 	}
+	
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer(){
+		return factory -> factory.configureDefault(id -> {
+					return new Resilience4JConfigBuilder(id)
+							.circuitBreakerConfig(CircuitBreakerConfig.custom()
+									.slidingWindowSize(10)
+									.failureRateThreshold(50)
+									.waitDurationInOpenState(Duration.ofSeconds(10L))
+									.build())
+							.timeLimiterConfig(TimeLimiterConfig.ofDefaults())
+							.build();
+				}); 
+	} 
+	
+	
+	
 	
 
 }
