f446a1eef7cc732900d1aa545fae1937b3fba396
David de la Rosa Delgado <drd_id@hotmail.com>
2022-01-10 21:41:39 +0100

    43. Configurando microservicio items con Resilience4J

--- poc-springboot-micro-item/src/main/java/org/delard/poc/springboot/micro/app/item/controllers/ItemRestController.java
+++ poc-springboot-micro-item/src/main/java/org/delard/poc/springboot/micro/app/item/controllers/ItemRestController.java
@@ -3,11 +3,13 @@ package org.delard.poc.springboot.micro.app.item.controllers;
 import java.util.List;
 
 import org.delard.poc.springboot.micro.app.item.models.Item;
+import org.delard.poc.springboot.micro.app.item.models.Producto;
 import org.delard.poc.springboot.micro.app.item.models.service.IItemsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -16,6 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ItemRestController {
+	
+	@Autowired
+	private CircuitBreakerFactory cbFactory;
 	
 	@Autowired
 	@Qualifier("serviceFeign")
@@ -41,8 +46,23 @@ public class ItemRestController {
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detail(@PathVariable Long id, @PathVariable Integer cantidad ) {
 		
-		return itemService.findById(id, cantidad);
+		// de forma programatica en caso de error se llama a unmetodo alternativo
+		return cbFactory.create("items")
+				.run(() -> itemService.findById(id, cantidad),
+						e-> metodoAlternativo(id, cantidad));
 		
 	}	
+
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
+		Producto producto = new Producto();
+		
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Camara Sony");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+		return item;
+	}
 	
 }
